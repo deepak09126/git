26c235d09314c0c6663b8611ba886785de514340
deepak kumar senapati <deepakkumarcbit9@gmail.com>
2025-04-04 15:34:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hi";
-    cout<<"how are u";
+   int arr[]={3,5,8,6,3,5};
+ int tar,sum=0;
+    for(i=0;arr[i]<tar:i++){
+        sum=sum+arr[i]
+            }
+    cout<<sum;
     return 0;
 }
